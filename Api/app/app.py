60463970a74418ad70b8f00113bd652f491e73a1
jose giron <josedanielgrn01@gmail.com>
2024-05-29 15:14:28 -0600

    manejo de usuarios y peliculas en la api

--- Api/app/app.py
+++ Api/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
-from login import obtener_usuarios
+from movies import obtener_peliculas
+from login import crear_user, obtener_user, obtener_usuarios
 
 app = Flask(__name__)
 
@@ -8,31 +9,42 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/api/login', methods=["POST"])
-def post_data():
-    if request.is_json:
-        data = request.get_json()
-        return jsonify({"you sent": data}), 201
+@app.route('/api/register', methods=["POST"])
+def post_new_user():
+    datos = request.get_json()
+    
+    name = datos.get('name')
+    lastname = datos.get('lastname')
+    user = datos.get('user')
+    password = datos.get('password')
+    level = datos.get('level')
+
+    success = crear_user(name, lastname, user, password, level)
+    if success:
+        return jsonify({"Mensaje": "Usuario creado correctamente"}), 201
     else:
-        return jsonify({"error": "Request must be JSON"}), 415
+        return jsonify({"error": "Server error"}), 500
+
+@app.route('/api/login', methods=["GET"])
+def login():
+    datos = request.get_json()
+
+    user = datos.get('user')
+    password = datos.get('password')
+
+    user_exist = obtener_user(user, password)
+
+    return user_exist
 
 @app.route('/api/allusers', methods=["GET"])
-def get_data():
+def get_users():
     usuarios = obtener_usuarios()
     return usuarios
 
-# @app.route('/api/data', methods=['GET'])
-# def get_data():
-#     data = {'key': 'value'}
-#     return jsonify(data)
-
-# @app.route('/api/data', methods=['POST'])
-# def post_data():
-#     if request.is_json:
-#         data = request.get_json()
-#         return jsonify({"you sent": data}), 201
-#     else:
-#         return jsonify({"error": "Request must be JSON"}), 415
+@app.route('/api/allmovies', methods=["GET"])
+def get_movies():
+    movies = obtener_peliculas()
+    return movies
 
 if __name__ == '__main__':
     app.run(debug=True)
