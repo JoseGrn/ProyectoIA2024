30ac16a60e2b2757b7d2ef939bedb3d831d42501
Andrea-Gudiel-Zepeda <andreamaria1d@gmail.com>
2024-05-30 02:33:04 -0600

    cambio login

--- Front/rottentomatoesgt/src/components/Login.jsx
+++ Front/rottentomatoesgt/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     event.preventDefault();
   
       fetch('http://127.0.0.1:5000/api/login', {
-      method: 'POST',
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
